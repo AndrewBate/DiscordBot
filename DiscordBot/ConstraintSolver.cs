fa03208eb392cd440b10f3e10f8f284375b8eb61
Andrew <andrew.james.bate@gmail.com>
2024-11-26 19:53:01 +0000

    Constraint Solver Skeleton

--- DiscordBot/ConstraintSolver.cs
+++ DiscordBot/ConstraintSolver.cs
@@ -1,38 +1,64 @@
-﻿using System;
+﻿using Discord;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordBot {
+
+    struct SquadRequirements {
+        public string name; // Used for lookup by user for appoprate role set (raid wind / fractal comp)
+        public List<Role> roles; // The roles that are valid
+        public Dictionary<string, int> attributeCount; // How much of heal/boons/dps are needed in total
+    };
+    struct Role {
+        public string name;
+        public string[] attributes;
+    }
+    struct Player {
+        public string name;
+        public Role[] Roles;
+    }
+    class util {
+        public static SquadRequirements basicRaidSquad = new SquadRequirements() {
+            name = "standard raid squad",
+            roles = new List<Role>() {
+                new Role() {
+                     name = "HealAlac",
+                     attributes = new string[] {"heal", "alacrity"}
+                },
+                new Role() {
+                    name = "HealQuick",
+                    attributes = new string[] {"heal", "quickness"}
+                },   
+                new Role {
+                    name = "AlacDPS",
+                    attributes = new string[] {"boondps", "alacrity"}
+                },
+                new Role {
+                    name = "QuickDPS",
+                    attributes = new string[] {"boondps", "quickness"}
+                },
+                new Role {
+                    name = "DPS",
+                    attributes = new string[] {"DPS"}
+                }
+
+            },
+            attributeCount = new Dictionary<string, int>() {
+                { "heal", 2 },
+                { "boondps", 2 },
+                { "alacrity", 2 },
+                { "quickness", 2 },
+                { "DPS", 2 }
+            },
+        };
+    }
+
+
     internal class ConstraintSolver {
-        uint nextAttributeIndex = 0;
-        Dictionary<String, int> attributeIndexes = new Dictionary<String, int>();
-
-        Dictionary<String, Player> players = new Dictionary<string, Player>();
-
-        ConstraintSolver(Dictionary<string, int> attributeCount, Role[] roles) {
-            foreach (var role in roles) {
-            }
-
-            foreach (var attribute in attributeCount) {
-            }
-        }
-
-        struct Role {
-            string name;
-            String[] attributes;
-        }
-
-        struct SquadComp {
-            string name;
-            Dictionary<String, int> attributeCounts;
-        }
-
-        struct Player {
-            string name;
-            List<Role> roles;
-        }
+        // Converts input contstraints to DancingLinks algorithm
 
         class DlxLinks {
             string _player;
